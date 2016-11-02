9eab92b429c4291570badb175b50a68ece5b80ea
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2016-11-02 15:52:59 +0100

    OnFragmentInteractionListener

---
+++ WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/listeners/OnFragmentInteractionListener.java
@@ -0,0 +1,8 @@
+package nmct.jaspernielsmichielhein.watchfriends.listeners;
+
+import android.net.Uri;
+
+public interface OnFragmentInteractionListener {
+
+    void onFragmentInteraction(Uri uri);
+}
