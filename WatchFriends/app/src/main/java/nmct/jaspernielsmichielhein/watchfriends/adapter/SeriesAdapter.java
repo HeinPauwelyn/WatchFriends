006769615b8fa494ad79140e8d72f3c9ed262af6
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2016-11-05 18:12:37 +0100

    :bug: opnieuw hetzelfde

--- WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/adapter/SeriesAdapter.java
+++ WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/adapter/SeriesAdapter.java
@@ -2,39 +2,54 @@ package nmct.jaspernielsmichielhein.watchfriends.adapter;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import nmct.jaspernielsmichielhein.watchfriends.R;
 import nmct.jaspernielsmichielhein.watchfriends.databinding.PosterSeriesBinding;
 import nmct.jaspernielsmichielhein.watchfriends.helper.Interfaces;
 import nmct.jaspernielsmichielhein.watchfriends.model.Series;
 
-public class SeriesAdapter extends ArrayAdapter<Series> {
+public class SeriesAdapter extends RecyclerView.Adapter<SeriesAdapter.SeriesViewHolder> {
     public Interfaces.onSeriesSelectedListener mListener;
 
-    public SeriesAdapter(Context context, ListView listView) {
-        super(context, R.layout.poster_series);
+    public SeriesAdapter(Context context, RecyclerView recyclerView) {
+
         mListener = (Interfaces.onSeriesSelectedListener) context;
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Series selectedSeries = getItem(position);
-                if (selectedSeries != null) {
-                    mListener.onSeriesSelected(selectedSeries);
-                }
-            }
-        });
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        final PosterSeriesBinding posterSeriesBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.poster_series, parent, false);
-        posterSeriesBinding.setSeries(getItem(position));
-        return posterSeriesBinding.getRoot();
+    public SeriesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.poster_series, parent, false);
+        return new SeriesViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(SeriesViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class SeriesViewHolder extends RecyclerView.ViewHolder {
+
+        public SeriesViewHolder(View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+
+                }
+            });
+        }
     }
 }
--- WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/view/HomeFragment.java
+++ WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/view/HomeFragment.java
@@ -3,6 +3,7 @@ package nmct.jaspernielsmichielhein.watchfriends.view;
 import android.app.Fragment;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,6 +27,9 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         FragmentHomeBinding fragmentHomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
         homeFragmentViewModel = new HomeFragmentViewModel(getActivity(), fragmentHomeBinding);
+
+        //RecyclerView rvRecommended = (RecyclerView) super.findViewById(R.layout.rvRecommended);
+
         return fragmentHomeBinding.getRoot();
     }
 
