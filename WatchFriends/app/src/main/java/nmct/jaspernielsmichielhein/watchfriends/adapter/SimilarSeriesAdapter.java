d428b2b2c7dcf5e672330b0cf57d939b5d558e8f
nielsbril <niels.bril@student.howest.be>
2016-11-07 11:00:12 +0100

    Similar series: row_series update

--- WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/adapter/SimilarSeriesAdapter.java
+++ WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/adapter/SimilarSeriesAdapter.java
@@ -2,16 +2,21 @@ package nmct.jaspernielsmichielhein.watchfriends.adapter;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+
+import com.squareup.picasso.Picasso;
 
 import nmct.jaspernielsmichielhein.watchfriends.R;
 import nmct.jaspernielsmichielhein.watchfriends.databinding.RowSeriesBinding;
+import nmct.jaspernielsmichielhein.watchfriends.helper.Contract;
 import nmct.jaspernielsmichielhein.watchfriends.helper.Interfaces;
 import nmct.jaspernielsmichielhein.watchfriends.model.Series;
 
@@ -40,6 +45,13 @@ public class SimilarSeriesAdapter extends ArrayAdapter<Series> {
     public View getView(int position, View convertView, ViewGroup parent) {
         final RowSeriesBinding rowSeriesBinding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.row_series, parent, false);
         rowSeriesBinding.setSeries(getItem(position));
+
+        Series series = getItem(position);
+        Uri uri = Uri.parse(Contract.MOVIEDB_IMAGE_BASE_URL + series.getPoster_path());
+
+        ImageView imgPoster = (ImageView) rowSeriesBinding.getRoot().findViewById(R.id.imgPoster);
+        Picasso.with(context).load(uri).into(imgPoster);
+
         return rowSeriesBinding.getRoot();
     }
 }
