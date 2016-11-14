672e50d65737d867507fc4b1082aad06966679a6
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2016-11-14 11:37:43 +0100

    :warning: problems met local host #105

--- WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/api/MovieDBService.java
+++ WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/api/MovieDBService.java
@@ -14,27 +14,24 @@ import rx.Observable;
 
 public interface MovieDBService {
     //SERIES
-    //https://api.themoviedb.org/3/tv/63174?api_key=1447c9e70c5784fbe8a492a4d5f37c8b&language=en-US
-    @GET("tv/{series}?api_key=" + Contract.MOVIEDB_API_KEY + "&language=en-US")
+    //https://api.themoviedb.org/3/tv/63174?api_key=1447c9e70c5784fbe8a492a4d5f37c8b&append_to_response=images,similar
+    @GET("tv/{series}?append_to_response=images,similar&api_key=" + Contract.MOVIEDB_API_KEY)
     Observable<Series> getSeries(@Path("series") int seriesId);
 
     //SIMILAR SERIES
     //https://api.themoviedb.org/3/tv/63174/similar?api_key=1447c9e70c5784fbe8a492a4d5f37c8b&language=en-US
-    @GET("tv/{series}/similar?api_key=" + Contract.MOVIEDB_API_KEY + "&language=en-US")
+    @GET("tv/{series}/similar?language=en-US&api_key=" + Contract.MOVIEDB_API_KEY)
     Observable<SimilarSeriesResult> getSimilarSeries(@Path("series") int seriesId);
 
     //SEASON
     //https://api.themoviedb.org/3/tv/63174/season/2?api_key=1447c9e70c5784fbe8a492a4d5f37c8b&language=en-US
-    @GET("tv/{series}/season/{season}?api_key=" + Contract.MOVIEDB_API_KEY + "&language=en-US")
+    @GET("tv/{series}/season/{season}?language=en-US&api_key=" + Contract.MOVIEDB_API_KEY)
     Observable<Season> getSeason(@Path("series") int seriesId, @Path("season") int season);
 
     //EPISODE
     //https://api.themoviedb.org/3/tv/63174/season/2/episode/2?api_key=1447c9e70c5784fbe8a492a4d5f37c8b&language=en-US
-    @GET("tv/{series}/season/{season}/episode/{episode}?api_key=" + Contract.MOVIEDB_API_KEY + "&language=en-US")
+    @GET("tv/{series}/season/{season}/episode/{episode}?language=en-US&api_key=" + Contract.MOVIEDB_API_KEY)
     Observable<Episode> getEpisode(@Path("series") int seriesId, @Path("season") int season, @Path("episode") int episode);
-
-    @GET("tv/{series}/images?api_key=" + Contract.MOVIEDB_API_KEY)
-    Observable<MediaPackage> getMediaSeries(@Path("series") int seriesId);
 
     @GET("list/{id}?api_key=" + Contract.MOVIEDB_API_KEY)
     Observable<SeriesList> getSeriesList(@Path("id") int id);
--- WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/helper/Contract.java
+++ WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/helper/Contract.java
@@ -7,5 +7,5 @@ public class Contract {
     public static final String MOVIEDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w342/";
 
     //WATCHFRIENDS
-    public static final String WATCHFRIENDS_BASE_URL = "http://localhost:8080/data/";
+    public static final String WATCHFRIENDS_BASE_URL = "http://172.30.248.101:8080/data/";
 }
