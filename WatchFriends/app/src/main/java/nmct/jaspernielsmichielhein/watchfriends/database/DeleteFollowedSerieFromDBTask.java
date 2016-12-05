01ca2d51a45b948fbb8aaa074ad75d6bdcf9205d
Michiel Vermeersch <mick.vermeersch@gmail.com>
2016-12-05 09:36:22 +0100

    Adding & Removing followedseries from SQLite db

--- WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/database/DeleteFollowedSerieFromDBTask.java
+++ WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/database/DeleteFollowedSerieFromDBTask.java
@@ -5,21 +5,26 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 
+import java.util.ArrayList;
+
 import nmct.jaspernielsmichielhein.watchfriends.provider.Contract;
 
 public class DeleteFollowedSerieFromDBTask extends AsyncTask<ContentValues, Void, Void> {
 
     private Context mContext;
-    public DeleteFollowedSerieFromDBTask(Context context) {mContext = context; }
+
+    public DeleteFollowedSerieFromDBTask(Context context) {
+        mContext = context;
+    }
 
     @Override
     protected Void doInBackground(ContentValues... params) {
+        String[] args = new String[]{String.valueOf(params[0].get(nmct.jaspernielsmichielhein.watchfriends.database.Contract.FollowedSeriesColumns.COLUMN_FOLLOWEDSERIES_NR))};
         mContext.getContentResolver().delete(
                 Contract.FOLLOWEDSERIES_URI,
-                nmct.jaspernielsmichielhein.watchfriends.database.Contract.FollowedSeriesColumns.COLUMN_FOLLOWEDSERIES_NR,
-                null
+                nmct.jaspernielsmichielhein.watchfriends.database.Contract.FollowedSeriesColumns.COLUMN_FOLLOWEDSERIES_NR + "=?",
+                args
         );
-        //Uri newUri = mContext.getContentResolver().delete(Contract.FOLLOWEDSERIES_URI, nmct.jaspernielsmichielhein.watchfriends.database.Contract.FollowedSeriesColumns.COLUMN_FOLLOWEDSERIES_NR, params);
         return null;
     }
 
