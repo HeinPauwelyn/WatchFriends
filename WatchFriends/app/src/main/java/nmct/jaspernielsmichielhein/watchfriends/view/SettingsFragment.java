a79a51356e3f220467e178c2db793283bcc1136f
Michiel Vermeersch <mick.vermeersch@gmail.com>
2016-12-21 16:21:22 +0100

    HeaderListener in settingsfragment

--- WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/view/SettingsFragment.java
+++ WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/view/SettingsFragment.java
@@ -1,12 +1,21 @@
 package nmct.jaspernielsmichielhein.watchfriends.view;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import nmct.jaspernielsmichielhein.watchfriends.R;
+import nmct.jaspernielsmichielhein.watchfriends.helper.Interfaces;
 
 public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private Context context;
+    private Interfaces.headerChangedListener headerListener;
+
     public SettingsFragment() {
 
     }
@@ -15,6 +24,19 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        context = getActivity();
+
+        if (context instanceof Interfaces.headerChangedListener) {
+            headerListener = (Interfaces.headerChangedListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement headerChangedListener");
+        }
+
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @Override
@@ -32,5 +54,11 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        headerListener.setAppBarBehavior(false);
     }
 }
