5fe05bedec3c31c3d44bf8209e14f6c9987afa27
Jasper De Sutter <jasper.de.sutter@student.howest.be>
2017-01-25 17:26:11 +0100

    Meer routes

--- WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/api/WatchFriendsService.java
+++ WatchFriends/app/src/main/java/nmct/jaspernielsmichielhein/watchfriends/api/WatchFriendsService.java
@@ -1,18 +1,24 @@
 package nmct.jaspernielsmichielhein.watchfriends.api;
 
+import com.android.annotations.Nullable;
 import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import nmct.jaspernielsmichielhein.watchfriends.model.Episode;
+import nmct.jaspernielsmichielhein.watchfriends.model.Follower;
 import nmct.jaspernielsmichielhein.watchfriends.model.Page;
 import nmct.jaspernielsmichielhein.watchfriends.model.SeriesList;
 import nmct.jaspernielsmichielhein.watchfriends.model.Season;
 import nmct.jaspernielsmichielhein.watchfriends.model.Series;
+import nmct.jaspernielsmichielhein.watchfriends.model.User;
+import nmct.jaspernielsmichielhein.watchfriends.model.UserData;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.PUT;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
@@ -30,9 +36,29 @@ public interface WatchFriendsService {
     @POST("auth/login")
     Call<JsonObject> login(@Field("email") String email, @Field("password") String password);
 
-    //HOME LISTS
+    //LISTS
     @GET("list")
     Observable<ArrayList<SeriesList>> getLists(@Query("access_token") String token);
+
+    //USERS
+    @GET("users/search/{query}")
+    Observable<ArrayList<User>> searchUsers(@Path("query") String query, @Query("access_token") String authToken);
+
+    @GET("users/{id}")
+    Observable<UserData> getUser(@Path("id") String userId, @Query("access_token") String authToken);
+
+    //FOLLOWERS
+    @GET("users/{id}/followers")
+    Observable<ArrayList<Follower>> getUserFollowers(@Path("id") String userId, @Query("access_token") String authToken);
+
+    @GET("users/{id}/follows")
+    Observable<ArrayList<Follower>> getUserFollows(@Path("id") String userId, @Query("access_token") String authToken);
+
+    @GET("users/{follower}/follows/{followed}")
+    Observable<Follower> getUserFollows(@Path("follower") String followerId, @Path("followed") String followedId, @Query("access_token") String authToken);
+
+    @PUT("users/{follower}/follows/{followed}")
+    Observable<Follower> updateUserFollows(@Path("follower") String followerId, @Path("followed") String followedId, @Field("following") Boolean following, @Query("access_token") String authToken);
 
     //SERIES
     @GET("series/{series}")
@@ -54,5 +80,8 @@ public interface WatchFriendsService {
     Observable<ArrayList<Series>> getFollowedSeries(@Query("user") String userId, @Query("access_token") String token);
 
     @GET("followed/{series}")
-    Observable<ArrayList<Series>> getFollowedSeries(@Query("user") String userId, @Path("series") int seriesId, @Query("access_token") String token);
+    Observable<Boolean> getFollowedSeries(@Query("user") String userId, @Path("series") int seriesId, @Query("access_token") String token);
+
+    @PUT("followed/{series}")
+    Observable<Boolean> UpdateFollowedSeries(@Query("user") String userId, @Path("series") int seriesId, @Field("following") Boolean following, @Query("access_token") String token);
 }
